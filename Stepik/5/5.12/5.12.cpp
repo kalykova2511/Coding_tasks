6b444af802a4809bc2c214fc8c5d95aee31ced69
Kristina Lykova <Kristina Lykova@KRISTINA>
2023-08-23 23:01:13 +0500

    5

---
+++ Stepik/5/5.12/5.12.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n, strike = 1, max_strike = 1, before_n = 0;
+
+    while (std::cin >> n && n != 0) {
+        if (n == before_n) {
+            ++strike;
+        }
+        else {
+            strike = 1;
+        }
+        if (strike > max_strike) {
+            max_strike = strike;
+        }
+        before_n = n;
+    }
+
+    std::cout << max_strike;
+
+    return 0;
+}
---
+++ Stepik/5/5.13/5.13.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n, counter = 0, before_n = INT16_MAX, before_n2 = INT16_MAX;
+
+    while (std::cin >> n && n != 0) {
+        if (before_n > n && before_n2 < before_n) {
+            ++counter;
+        }
+        before_n2 = before_n;
+        before_n = n;
+    }
+
+    std::cout << counter;
+
+    return 0;
+}
